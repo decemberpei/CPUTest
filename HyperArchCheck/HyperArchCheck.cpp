ed343a910770a64ec8e3dfb2d81409e2ec23ae12
decemberpei <decemberpei@gmail.com>
2022-11-01 14:04:48 +0800

    add utility to check cpu arc info (hyper thread and heterogeneous)

---
+++ HyperArchCheck/HyperArchCheck.cpp
@@ -0,0 +1,45 @@
+#include <Windows.h>
+
+#include <string.h>
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main(void)
+{
+	SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX* processInfoBuffer = NULL;
+	DWORD bufferLen = 0;
+
+	if (FALSE == GetLogicalProcessorInformationEx(RelationProcessorCore, processInfoBuffer, &bufferLen)){
+		// expected "false" return value
+		if (bufferLen <= 0) {
+			cout << "GetLogicalProcessorInformationEx failed with error " << GetLastError() << endl;
+			return -1;
+		}
+	}
+	processInfoBuffer = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX*) new char[bufferLen];
+	if (FALSE == GetLogicalProcessorInformationEx(RelationProcessorCore, processInfoBuffer, &bufferLen)) {
+		cout << "GetLogicalProcessorInformationEx failed with error " << GetLastError() << endl;
+		return -1;
+	}
+
+	SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX* tmpProcessorInfo = processInfoBuffer;
+	int offset = 0;
+	int processInfoCount = 0;
+	while (true){
+		PROCESSOR_RELATIONSHIP pr = tmpProcessorInfo->Processor;
+		cout << "core index: " << std::setw(2) <<processInfoCount;
+		cout << ", hyper thread support: " << (pr.Flags == LTP_PC_SMT) ? "Yes" : "No";
+		cout << ", heterogeneous platform: " << (pr.EfficiencyClass != 0) ? "Yes" : "No";
+		cout << endl;
+		processInfoCount++;
+		offset = offset + tmpProcessorInfo->Size;
+		tmpProcessorInfo = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX*)((char*)processInfoBuffer + offset);
+		if (offset >= bufferLen) {
+			break;
+		}
+	}
+	delete []processInfoBuffer;
+	return 0;
+}
